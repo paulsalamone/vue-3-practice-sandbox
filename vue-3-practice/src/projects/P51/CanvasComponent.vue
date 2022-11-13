7a58ca34e2c87da3a268b755b4ebdf2fbb98ac22
Paul Salamone <93609282+paulsalamone@users.noreply.github.com>
2022-11-13 15:21:55 +0100

    basic p5 + slider

---
+++ vue-3-practice/src/projects/P51/CanvasComponent.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <h2>{{ props.circleWidth }}</h2>
+    <div id="vue-canvas"></div>
+  </div>
+</template>
+
+<script setup>
+import p5 from "p5";
+import { onMounted } from "vue";
+
+onMounted(() => {
+  const script = function (p5) {
+    p5.setup = () => {
+      const canvas = p5.createCanvas(500, 500);
+      canvas.parent("vue-canvas");
+    };
+
+    // let spacer = 1;
+
+    p5.draw = () => {
+      p5.background(5);
+      p5.fill(66, 124, 31);
+      p5.stroke(270);
+      p5.strokeWeight(10);
+
+      p5.ellipse(250, 250, props.circleWidth, props.circleWidth);
+
+      //   spacer++;
+      //   if (spacer > 500) {
+      //     spacer = 0;
+      //   }
+    };
+  };
+
+  const P5 = p5;
+  new P5(script);
+});
+const props = defineProps({
+  circleWidth: Number,
+});
+</script>
+
+<style scoped>
+#vue-canvas {
+  /* display: block;
+  margin: 0 auto;
+  padding: 0; */
+  width: 500px;
+  height: 500px;
+  overflow: hidden;
+}
+</style>
--- vue-3-practice/src/views/P51View.vue
+++ vue-3-practice/src/views/P51View.vue
@@ -1,7 +1,48 @@
 <template>
   <h1>p5 1</h1>
+  <div class="p5-container">
+    <h3>{{ circleWidth.value }}</h3>
+    <!-- <ToolsLeft :circleWidth="circleWidth" /> -->
+    <div class="tools">
+      <form action="">
+        <input
+          type="range"
+          min="1"
+          max="500"
+          :value="circleWidth.value"
+          @change="handleWidth()"
+        />
+      </form>
+    </div>
+    <CanvasComponent :circleWidth="circleWidth" />
+  </div>
 </template>
 
-<script></script>
+<script setup>
+import CanvasComponent from "../projects/P51/CanvasComponent.vue";
+import { ref } from "vue";
+const circleWidth = ref(300);
+function handleWidth() {
+  circleWidth.value = event.target.value;
+  console.log(circleWidth);
+}
 
-<style></style>
+// < input
+// type="range"
+// min="0"
+// v-bind:max="max"
+// v-bind:value="tmpVal"
+// v-on:input=“onChg($event)”>
+
+// onChg() {
+// this.tmpVal = evt.target.value
+// }
+</script>
+
+<style scoped>
+.p5-container {
+  display: grid;
+  grid-template-columns: 1fr 4fr;
+  grid-gap: 10px;
+}
+</style>
